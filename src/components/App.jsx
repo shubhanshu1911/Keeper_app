a68552f2e925400154dacb2d9b205c9328655888
shubhanshu khare <shubhkhare19@gmail.com>
2023-12-13 15:28:47 +0530

    Part 3 Codes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note.jsx";
-import notes from "../notes";
-
-function createNote(note) {
-  return <Note key={note.id} title={note.title} content={note.content} />;
-}
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Header />
-      {notes.map(createNote)};
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, noteIndex) => {
+        return (
+          <Note
+            key={noteIndex}
+            id={noteIndex}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
